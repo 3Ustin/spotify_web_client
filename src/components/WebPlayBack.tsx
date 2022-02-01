13dec13922f89ae3478908d1ab95d3e6aac90fd4
Austin D Dupras <79591045+3Ustin@users.noreply.github.com>
2022-01-31 19:06:24 -0500

    Code Clean Up
    The WebPlayBack.js was deleted. That file was for testing purposes and for a smooth transition to a .tsx file. Other than validation of the token key from Spotify and the ts-ignores the WebPlayback was added successfully.

--- src/components/WebPlayBack.tsx
+++ src/components/WebPlayBack.tsx
@@ -1,40 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import player from '../utilities/WebPlayback';
+import { useEffect } from 'react';
 
-//Props are put in here to pass the Access Token, but Mile's might have a different method.
+//In current build put Access Token Manually in (Line 18)
+//Look for an issue in Git for authentification for more info.
 function WebPlayback(props: any) {
-    const [player, setPlayer] = useState(undefined);
-
 
     useEffect(() => {
 
+        //For importing Spotify SDK.
         const script = document.createElement("script");
         script.src = "https://sdk.scdn.co/spotify-player.js";
         script.async = true;
-
         document.body.appendChild(script);
-        //Turned this into an any type becuase I think Typescript is 'acting up' here.
-        (window as any).onSpotifyWebPlaybackSDKReady = () => {
-            //EXPERIMENTING WITH OUT TO USE TYPE SCRIPT
-        //window.(onSpotifyWebPlaybackSDKReady: Boolean) = () => {
-            //TOKENS will be passed through props unless Mile's says otherwise this is just for testing purposes.
-            const token = 'MY ACCESS TOKEN';
-            const player = new (window as any).Spotify.Player({
-                name: 'Web Playback SDK',
-                getOAuthToken: (cb: any) => { cb(token); },
+
+        //'ts-ignore' is a Bandaid fix see issue on GitHub to create a 'Type Declaration File'.
+        // @ts-ignore
+        window.onSpotifyWebPlaybackSDKReady = () => {
+            const token = 'My Access Token';
+            //@ts-ignore
+            //For instantiating Spotify Player object.
+            const player = new window.Spotify.Player({
+                name: 'WebPlayBack SDK',
+                getOAuthToken: (cb: (arg:string) => any) => { cb(token); },
                 volume: 0.5
             });
-            //Hook call for player object storage.
-            setPlayer(player);
-            let device_id = Number;
-            player.addListener('ready', ({ device_id}) => {
-                console.log('Ready with Device ID', (device_id as number));
+
+            player.addListener('ready', ({device_id}: {device_id: number}) => {
+                console.log('Ready with Device ID', (device_id));
             });
 
-            player.addListener('not_ready', ({ device_id }) => {
+            player.addListener('not_ready', ({device_id}: {device_id: number}) => {
                 console.log('Device ID has gone offline', device_id);
             });
-
 
             player.connect();
 
