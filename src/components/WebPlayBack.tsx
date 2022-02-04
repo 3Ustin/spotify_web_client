8917e3eaed269469f3bd8a7d0dee623f37d2e394
Austin D Dupras <79591045+3Ustin@users.noreply.github.com>
2022-02-04 13:08:53 -0500

    State Hook Problem Review
    Implementing the useState hook to store and update the player object. Method not permanent.

--- src/components/WebPlayBack.tsx
+++ src/components/WebPlayBack.tsx
@@ -1,9 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useState} from 'react';
 
 //In current build put Access Token Manually in (Line 18)
 //Look for an issue in Git for authentification for more info.
 function WebPlayback(props: any) {
-
+    var [statePlayer, setPlayer] = useState();
     useEffect(() => {
 
         //For importing Spotify SDK.
@@ -16,30 +16,38 @@ function WebPlayback(props: any) {
         // @ts-ignore
         window.onSpotifyWebPlaybackSDKReady = () => {
             const token = 'My Access Token';
-            //@ts-ignore
             //For instantiating Spotify Player object.
+            //@ts-ignore
             const player = new window.Spotify.Player({
                 name: 'WebPlayBack SDK',
                 getOAuthToken: (cb: (arg:string) => any) => { cb(token); },
                 volume: 0.5
             });
-
+            
             //Device ID is returning a string I believe not a number, as miles about this.
+            //@ts-ignore
             player.addListener('ready', ({device_id}: {device_id: number}) => {
                 console.log('Ready with Device ID', (device_id));
             });
-
+            //@ts-ignore
             player.addListener('not_ready', ({device_id}: {device_id: number}) => {
                 console.log('Device ID has gone offline', device_id);
             });
-
+            //@ts-ignore
             player.connect();
 
+            setPlayer({statePlayer: player});
         };
     }, []);
 
     const buttonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
-        console.log("clicked");
+        //@ts-ignore
+        console.log(statePlayer);
+        //@ts-ignore
+        statePlayer.getVolume().then((volume: number)=> {
+            let volume_percentage = volume * 100;
+            console.log(`The volume of the player is ${volume_percentage}%`);
+        });
     }
 
     return (
