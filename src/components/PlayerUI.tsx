67d0f79eb31e89d42a2d7a4e9a371642d67c31cb
Austin D Dupras <79591045+3Ustin@users.noreply.github.com>
2022-02-22 20:05:07 -0500

    Comments
    This is a commit after a pull request but before resolution.

--- src/components/PlayerUI.tsx
+++ src/components/PlayerUI.tsx
@@ -1,20 +1,23 @@
 import { useState, useEffect} from 'react';
 import WebPlayback from './WebPlayBack';
 
+//Acceptable format for props data.
 export interface IPlayerUIProps {
     trackName: string,
     albumName: string,
     artistName: string
 }
-
 //@ts-ignore
 function PlayerUI(props: IPlayerUIProps) {
-    //Is this Deconstruction?
+
+    //Deconstruction props
     const {
         trackName,
         albumName,
         artistName
     } = props;
+
+    //Displaying to the user props.
     return(
         <div> 
             <div>Name: {trackName}</div>
