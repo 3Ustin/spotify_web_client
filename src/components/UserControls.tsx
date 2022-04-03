13213f61797ee83e10321042daaf6900b860c6de
Austin D Dupras <79591045+3Ustin@users.noreply.github.com>
2022-04-03 10:27:36 -0400

    Adding MaterialUI to Player Controls.
    We added MaterialUI to the project and gave player controls style.

--- src/components/UserControls.tsx
+++ src/components/UserControls.tsx
@@ -1,22 +1,46 @@
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import Button from '@mui/material/Button';
+import Slider from '@mui/material/Slider';
+import FastRewind from '@mui/icons-material/FastRewind';
+import Replay10Icon from '@mui/icons-material/Replay10';
+import Forward10Icon from '@mui/icons-material/Forward10';
+import PauseIcon from '@mui/icons-material/Pause';
+
 //Acceptable format for props data
-export interface IUserControlsProps {
-    backward: (event: React.MouseEvent<HTMLElement>) => void
+export interface IUserControlFunctions {
+    backward: (event: React.MouseEvent<HTMLElement>) => void,
     toggle: (event: React.MouseEvent<HTMLElement>) => void,
     forward: (event: React.MouseEvent<HTMLElement>) => void,
     volume: (event: React.ChangeEvent<HTMLInputElement>) => void,
     tenForward: (event: React.MouseEvent<HTMLElement>) => void,
-    tenBackward: (event: React.MouseEvent<HTMLElement>) => void
+    tenBackward: (event: React.MouseEvent<HTMLElement>) => void,
+}
+
+export interface IUserControlsProps {
+    controlFunctions: IUserControlFunctions,
+    isPlaying: boolean
 }
 function UserControls(props:IUserControlsProps){
+//PUT IN BOOL LOGIC HERE 
+    let toggleIcon;
+    if(props.isPlaying){toggleIcon = <PauseIcon/>}
+    else{toggleIcon = <PlayArrowIcon/>}
+
+
     //Displaying Props
 return(
     <div> 
-        <button onClick={props.backward}>backward</button>
-        <button onClick={props.toggle}>toggle</button>
-        <button onClick={props.forward}>forward</button>
-        <input type="range" min="0" max="100" step="1" onChange={props.volume}></input>
-        <button onClick={props.tenBackward}>-10s</button>
-        <button onClick={props.tenForward}>+10s</button>
+        <Button onClick={props.controlFunctions.backward} variant="contained" startIcon={<SkipPreviousIcon/>}>backward</Button>
+        <Button onClick={props.controlFunctions.toggle} variant="contained" startIcon={toggleIcon}></Button>
+        <Button onClick={props.controlFunctions.forward} variant="contained" startIcon={<SkipNextIcon/>}>forward</Button>
+        {/*@ts-ignore*/}
+        <Slider min={1} max={100} step={1} onChange={props.controlFunctions.volume}></Slider>
+        <Button onClick={props.controlFunctions.tenBackward} variant="contained" startIcon={<Replay10Icon/>}>-10s</Button>
+        <Button onClick={props.controlFunctions.tenForward} variant="contained" startIcon={<Forward10Icon/>}>+10s</Button>
     </div>
 );
 }
