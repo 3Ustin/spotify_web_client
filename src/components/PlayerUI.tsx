172fe7ebe1d23435087d2fb128437adac4ff7363
Austin D Dupras <79591045+3Ustin@users.noreply.github.com>
2022-04-25 12:00:09 -0400

    Displayed current Track Album and Artist on left of screen
    We decided to get the outline of these UI components before getting in the greater detail. This PR is an example of that strategy.

--- src/components/PlayerUI.tsx
+++ src/components/PlayerUI.tsx
@@ -1,3 +1,6 @@
+import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import React from "react";
+
 //Acceptable format for props data
 export interface IPlayerUIProps {
     trackName: string,
@@ -13,11 +16,13 @@ function PlayerUI(props: IPlayerUIProps) {
     } = props;
     //Displaying to the user props
     return(
-        <div> 
-            <div>Name: {trackName}</div>
-            <div>Album: {albumName}</div>
-            <div>Artist: {artistName}</div>
-        </div>
+        <AppBar position="fixed" color="primary" sx={{ left: '0', width:'200px', maxheight:'100%',height:'100%' }}>
+            <Toolbar sx={{ flexDirection:"column" }}>
+                <Container  sx={{padding:'20px', paddingTop:"40px"}}><Typography variant="h5" align="center" sx={{color:'rgb(0, 0, 0)'}}>Name: </Typography><Typography align="center" sx={{color:'rgb(0, 0, 0)'}}>{trackName}</Typography></Container>
+                <Container sx={{padding:'20px'}}><Typography variant="h5" align="center" sx={{color:'rgb(0, 0, 0)'}}>Album: </Typography><Typography align="center" sx={{color:'rgb(0, 0, 0)'}}>{albumName}</Typography></Container>
+                <Container sx={{padding:'20px'}}><Typography variant="h5" align="center" sx={{color:'rgb(0, 0, 0)'}}>Artist: </Typography><Typography align="center" sx={{color:'rgb(0, 0, 0)'}}>{artistName}</Typography></Container>
+            </Toolbar>
+        </AppBar>
     );
 }
 export default PlayerUI;
