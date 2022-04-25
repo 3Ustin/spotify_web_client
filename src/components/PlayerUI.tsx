07caadcad772be9eab8e636136468986e192cfa9
Austin D Dupras <79591045+3Ustin@users.noreply.github.com>
2022-04-25 17:59:48 -0400

    Changed updated for PR
    We decided having Uniform Quotations across the platform would be best.

--- src/components/PlayerUI.tsx
+++ src/components/PlayerUI.tsx
@@ -1,5 +1,5 @@
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
-import React from "react";
+import { AppBar, Container, Toolbar, Typography } from '@mui/material'
+import React from 'react'
 
 //Acceptable format for props data
 export interface IPlayerUIProps {
@@ -16,11 +16,11 @@ function PlayerUI(props: IPlayerUIProps) {
     } = props;
     //Displaying to the user props
     return(
-        <AppBar position="fixed" color="primary" sx={{ left: '0', width:'200px', maxheight:'100%',height:'100%' }}>
+        <AppBar position='fixed' color='primary' sx={{ left: '0', width:'200px', maxheight:'100%',height:'100%' }}>
             <Toolbar sx={{ flexDirection:"column" }}>
-                <Container  sx={{padding:'20px', paddingTop:"40px"}}><Typography variant="h5" align="center" sx={{color:'rgb(0, 0, 0)'}}>Name: </Typography><Typography align="center" sx={{color:'rgb(0, 0, 0)'}}>{trackName}</Typography></Container>
-                <Container sx={{padding:'20px'}}><Typography variant="h5" align="center" sx={{color:'rgb(0, 0, 0)'}}>Album: </Typography><Typography align="center" sx={{color:'rgb(0, 0, 0)'}}>{albumName}</Typography></Container>
-                <Container sx={{padding:'20px'}}><Typography variant="h5" align="center" sx={{color:'rgb(0, 0, 0)'}}>Artist: </Typography><Typography align="center" sx={{color:'rgb(0, 0, 0)'}}>{artistName}</Typography></Container>
+                <Container  sx={{padding:'20px', paddingTop:'40px'}}><Typography variant='h5' align='center' sx={{color:'rgb(0, 0, 0)'}}>Name: </Typography><Typography align='center' sx={{color:'rgb(0, 0, 0)'}}>{trackName}</Typography></Container>
+                <Container sx={{padding:'20px'}}><Typography variant='h5' align='center' sx={{color:'rgb(0, 0, 0)'}}>Album: </Typography><Typography align='center' sx={{color:'rgb(0, 0, 0)'}}>{albumName}</Typography></Container>
+                <Container sx={{padding:'20px'}}><Typography variant='h5' align='center' sx={{color:'rgb(0, 0, 0)'}}>Artist: </Typography><Typography align='center' sx={{color:'rgb(0, 0, 0)'}}>{artistName}</Typography></Container>
             </Toolbar>
         </AppBar>
     );
