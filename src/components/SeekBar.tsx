c550f87bcfbd400c3476ec40e1ea55f908559ee1
Miles Martin <mrmartin@mtu.edu>
2022-05-31 17:43:25 -0500

    Add debounce to prevent seek changes from spamming Spotify with seek requests

--- src/components/SeekBar.tsx
+++ src/components/SeekBar.tsx
@@ -4,6 +4,8 @@ import { DoubleArrowTwoTone } from '@mui/icons-material';
 import Slider from '@mui/material/Slider';
 import useInterval from '../hooks/useInterval';
 
+const SEEK_DEBOUNCE_TIMEOUT_MS = 500
+
 export interface SeekBarProps {
     isSongPlaying:boolean;
     duration:number;
@@ -14,25 +16,37 @@ export interface SeekBarProps {
 function SeekBar(props: SeekBarProps){
     const {isSongPlaying, duration, position, seek} = props;
     const [seekValue,setSeekValue] = useState<number>(position);
-
-    //Proccess B.1
-
-    //Proccess B.3
+    const [debounceTimeout, setDebounceTimeout] = useState<ReturnType<typeof setTimeout> | undefined>()
 
     //First time Component is Rendered run this function
     useInterval(processC,1000);
     function processC(){
-        if(isSongPlaying){setSeekValue(seekValue + 1000);}
+        console.warn(debounceTimeout)
+        if (isSongPlaying && debounceTimeout === undefined) {
+            setSeekValue(seekValue + 1000);
+        }
     }
     useEffect(() => {
-        setSeekValue(position)
+        setSeekValue(position);
     }, [duration, position]);
     function onChange(event:React.ChangeEvent<HTMLInputElement>){
-        console.log(event);
         const pos = parseInt(event.target.value);
-        seek(pos);
+        debouncedSeek(pos);
         setSeekValue(pos);
     }
+
+    const debouncedSeek = (pos: number) => {
+        if (debounceTimeout) {
+            clearTimeout(debounceTimeout);
+        }
+        const updatedTimeout = setTimeout(() => {
+            seek(pos);
+            setDebounceTimeout(undefined);
+        }, SEEK_DEBOUNCE_TIMEOUT_MS);
+
+        setDebounceTimeout(updatedTimeout);
+    };
+
     return(
     <Slider
         min={1}
